8cadab6aa4ae6019b899811fec26ab653b9812d1
vagrant <vagrant@rails-dev-box>
2013-12-13 17:44:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foos::Application.config.secret_key_base = 'c055d2f1b2c059b6073c595a9da9d9aec158acbdea47f2776423c9ec5a232ea9318ca6b714792937ad92d12b3f80a0a6d8cb2f882cc66daf82a7e1d2b76eb0ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foos::Application.config.session_store :cookie_store, key: '_foos_session'
