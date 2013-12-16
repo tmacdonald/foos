968bebd869e7f932bf4071020be560f82ee6a21f
Tim Macdonald <macdonald.tim@gmail.com>
2013-12-16 17:48:54 -0500

    Continued work on the team controller

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,20 +1,57 @@
 class TeamController < ApplicationController
+
+  before_action :set_team, :only[:show, :edit, :update, :destroy]
+
+  # GET /teams
   def index
-    
+    @teams = Team.all
   end
 
+  # GET /teams/new
   def new
+    @team = Team.new
   end
 
+  # GET /teams/1/edit
   def edit
   end
 
+  #GET /teams/1
+  def show
+  end
+
+  # POST /teams
   def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      redirect_to @team, notice: 'Team was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
+  # PUT/PATCH /teams/1
   def update
+    if @team.update(team_params)
+      redirect_to @team, notice: 'Team was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
+  # DELETE /teams/1
   def destroy
+    @team.destroy
+    redirect_to teams_url
   end
+
+  private
+    def set_team
+      @team = Team.find(params[:id])
+    end
+
+    def team_params
+      params.require(:team).permit(:name)
+    end
 end
