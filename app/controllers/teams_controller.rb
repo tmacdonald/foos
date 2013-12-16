908d3b8475464db452fcd25cdffd45b121f2d3d0
Tim Macdonald <macdonald.tim@gmail.com>
2013-12-16 18:16:42 -0500

    Got index and new team screens working

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
-class TeamController < ApplicationController
+class TeamsController < ApplicationController
 
-  before_action :set_team, :only[:show, :edit, :update, :destroy]
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
 
   # GET /teams
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Foos::Application.routes.draw do
-  get "team/index"
-  get "team/new"
-  get "team/edit"
-  get "team/create"
-  get "team/update"
-  get "team/destroy"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+  resources :teams
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
