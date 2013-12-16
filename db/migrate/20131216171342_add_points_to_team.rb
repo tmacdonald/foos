298162bfeff8731b43c988f3401529e5ff6ac6f1
Tim Macdonald <macdonald.tim@gmail.com>
2013-12-16 12:14:39 -0500

    Added points to team model

---
+++ db/migrate/20131216171342_add_points_to_team.rb
@@ -0,0 +1,5 @@
+class AddPointsToTeam < ActiveRecord::Migration
+  def change
+	add_column :teams, :points, :integer, :default => 500
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216025607) do
+ActiveRecord::Schema.define(version: 20131216171342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20131216025607) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "points",     default: 500
   end
 
   create_table "users", force: true do |t|
