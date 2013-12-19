5182035d7e7177e988b2063a21acf899545db6f9
Tim Macdonald <macdonald.tim@gmail.com>
2013-12-18 20:46:00 -0500

    Calculate alternate ranking points

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -26,7 +26,23 @@ class GamesController < ApplicationController
     @game = Game.new(game_params)
 
     if @game.save
-      redirect_to @game, notice: 'Game was successfully created.'
+
+      point_change = calculate_points_change(@game.team1.points, @game.team2.points, @game.team1score, @game.team2score)
+
+      logger.debug "Point change: #{point_change}"
+
+      if (@game.team1score > @game.team2score && @game.team1.ladder_rank > @game.team2.ladder_rank) ||
+        (@game.team1score < @game.team2score && @game.team1.ladder_rank < @game.team2.ladder_rank)
+
+          temp = @game.team1.ladder_rank
+          @game.team1.update(ladder_rank: @game.team2.ladder_rank)
+          @game.team2.update(ladder_rank: temp)
+      end
+
+      @game.team1.update(points: @game.team1.points + point_change)
+      @game.team2.update(points: @game.team2.points - point_change)
+
+      redirect_to teams_url, notice: 'Game was successfully created.'
     else
       render action: 'new'
     end
@@ -54,5 +70,21 @@ class GamesController < ApplicationController
 
     def game_params
       params.require(:game).permit(:team1_id, :team2_id, :team1score, :team2score)
+    end
+
+    def calculate_points_change (points1, points2, score1, score2)
+      w = score1 > score2 ? 1 : 0
+      gd = (score1 - score2).abs
+
+      g = (11 + gd) / 8.0
+      g = 1 if gd == 1
+      g = 1.5 if gd == 2
+
+      we = 1.0 / ((10 ** (-(points1 - points2)/400.0)) + 1)
+      k = 20
+
+      logger.debug "#{w}, #{g}, #{w}, #{we}"
+
+      (k * g * (w - we)).ceil
     end
 end
