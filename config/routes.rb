2b9e7dd30df3f11bf577ac666d77a46ec16e44ed
Tim Macdonald <macdonald.tim@gmail.com>
2013-12-19 22:40:37 -0500

    Change root to ladder

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Foos::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'teams#ladder'
 
   resources :teams
 
