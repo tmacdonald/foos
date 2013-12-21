88c20e306df22c08d8fb22ef30c60c4262ad9c22
Tim Macdonald <macdonald.tim@gmail.com>
2013-12-20 22:33:24 -0500

    Got SPA working for team index, ladder, rankings, and show
    Removed turbolinks

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,4 @@
-var app = angular.module('foos.app', ['ngRoute', 'ngResource', 'foos.teams']);
+var app = angular.module('foos.app', ['ngRoute', 'ngResource', 'ngAnimate', 'foos.teams']);
 
 app.config(function($routeProvider) {
   $routeProvider.otherwise({ redirectTo: '/teams' });
@@ -8,33 +8,31 @@ var teamModule = angular.module('foos.teams', ['foos.teams.controllers', 'foos.t
   .config(function($routeProvider) {
     $routeProvider
       .when('/teams/new', { templateUrl: '/assets/teams/form.html', controller: 'NewTeamController' })
-      .when('/teams/:id', { templateUrl: '/assets/teams/show.html', controller: 'TeamController' })
-      .when('/teams', { templateUrl: '/assets/teams/index.html', controller: 'TeamsController' })
-      .when('/ladder', { templateUrl: '/assets/teams/ladder.html', controller: 'TeamsController' })
-      .when('/rankings', { templateUrl: '/assets/teams/rankings.html', controller: 'TeamsController' });
+      .when('/teams/:id', { templateUrl: '/assets/teams/show.html' })
+      .when('/teams', { templateUrl: '/assets/teams/index.html' })
+      .when('/ladder', { templateUrl: '/assets/teams/ladder.html'})
+      .when('/rankings', { templateUrl: '/assets/teams/rankings.html' });
   });
 
 angular.module('foos.teams.controllers', [])
-  .controller('LadderController', function($scope, TeamService) {
+  .controller('TeamsController', function($scope, $routeParams, TeamService) {
+    $scope.find = function(query) {
+      TeamService.query(query).$promise.then(function(teams) {
+        $scope.teams = teams;
+      });
+    };
 
-  })
-  .controller('TeamsController', function($scope, TeamService) {
-    $scope.teams = [];
-
-    TeamService.query().$promise.then(function(data) { 
-      $scope.teams = data;
-    });
-  })
-  .controller('TeamController', function($scope, $routeParams, TeamService) {
-    TeamService.get({ id: $routeParams.id }).$promise.then(function(data) {
-      $scope.team = data;
-    });
+    $scope.findOne = function() {
+      TeamService.get({ id: $routeParams.id }).$promise.then(function(team) {
+        $scope.team = team;
+      });
+    };
   })
   .controller('NewTeamController', function($scope) {
 
   });
 
 angular.module('foos.teams.services', ['ngResource'])
-  .factory('TeamService', function($resource) {
+  .factory('TeamService', ['$resource', function($resource) {
     return $resource('/api/teams/:id', { id: '@id' });
-  });
+  }]);
