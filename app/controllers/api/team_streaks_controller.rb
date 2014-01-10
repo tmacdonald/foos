52bfb592dcf170be1206325d8e72cc76fa4173e3
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-10 10:37:02 -0500

    Fixed team streaks calculation

--- app/controllers/api/team_streaks_controller.rb
+++ app/controllers/api/team_streaks_controller.rb
@@ -9,23 +9,22 @@ class Api::TeamStreaksController < ApplicationController
   # GET /teams/:team_id/streaks/current
   def current
     streak = 0
-    game = Game.where("team1_id = ? OR team2_id = ?", params[:team_id], params[:team_id]).order(created_at: :desc).last
-    logger.debug game.inspect
+    team_id = params[:team_id].to_i
+    game = Game.where("team1_id = ? OR team2_id = ?", team_id, team_id).order(created_at: :desc).last
     unless game.nil?
-      if game.team1_id == params[:team_id]
-        last_loss = Game.where(:team2_id => params[:team_id]).order(created_at: :desc).last
-        logger.debug last_loss.inspect
+      if game.team1_id == team_id
+        last_loss = Game.where(:team2_id => team_id).order(created_at: :desc).last
         if last_loss.nil?
-          streak = Game.where("team1_id = ? OR team2_id = ?", params[:team_id], params[:team_id]).count
+          streak = Game.where("team1_id = ? OR team2_id = ?", team_id, team_id).count
         else
-          streak = Game.where("team1_id = ?", params[:team_id]).where('created_at > ?', last_loss.created_at).count
+          streak = Game.where("team1_id = ?", team_id).where('created_at > ?', last_loss.created_at).count
         end
       else
-        last_win = Game.where(:team1_id => params[:team_id]).order(created_at: :desc).last
+        last_win = Game.where(:team1_id => team_id).order(created_at: :desc).last
         if last_win.nil?
-          streak = -Game.where("team1_id = ? OR team2_id = ?", params[:team_id], params[:team_id]).count
+          streak = -Game.where("team1_id = ? OR team2_id = ?", team_id, team_id).count
         else
-          streak = -Game.where("team2_id = ?", params[:team_id]).where('created_at > ?', last_win.created_at).count
+          streak = -Game.where("team2_id = ?", team_id).where('created_at > ?', last_win.created_at).count
         end
       end
     end
