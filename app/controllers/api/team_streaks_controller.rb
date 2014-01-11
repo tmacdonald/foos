52ba3328b19b0247cdb3f19f96d404cbc49b0ff5
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-10 21:19:12 -0500

    Support for list of streaks

--- app/controllers/api/team_streaks_controller.rb
+++ app/controllers/api/team_streaks_controller.rb
@@ -2,19 +2,55 @@ class Api::TeamStreaksController < ApplicationController
 
   # GET /teams/:team_id/streaks
   def index
-    @games = Game.where("team1_id = ? OR team2_id = ?", params[:team_id], params[:team_id]).includes(:team1, :team2).order(created_at: :desc)
-    render 'api/games/index'
+
+    minimum_streak = 2
+    unless params[:minimum].nil?
+      minimum_streak = params[:minimum].to_i
+    end
+
+    unless params[:limit].nil?
+      limit = params[:limit].to_i
+      params.delete :limit
+    end
+
+    games = Game.filter(params).where("team1_id = ? OR team2_id = ?", params[:team_id], params[:team_id])
+    
+    team_id = params[:team_id].to_i
+    current_streak = 0
+    streaks = Array.new
+
+    games.each do |game|
+      logger.debug game.team1_id
+      unless current_streak == 0
+        if current_streak > 0 && game.team1_id == team_id
+          current_streak = current_streak + 1
+        elsif current_streak < 0 && game.team2_id == team_id
+          current_streak = current_streak - 1
+        else
+          streaks.push current_streak if current_streak.abs >= minimum_streak
+          current_streak = (game.team1_id == team_id) ? 1 : -1
+          if streaks.length == limit
+            break
+          end
+        end 
+      else
+        current_streak = (game.team1_id == team_id) ? 1 : -1
+      end
+    end
+    streaks.push current_streak if streaks.length < limit
+
+    render :json => streaks
   end
 
   # GET /teams/:team_id/streaks/current
   def current
     streak = 0
     team_id = params[:team_id].to_i
-    streak_scope = scope params
-    game = streak_scope.where("team1_id = ? OR team2_id = ?", team_id, team_id).order(created_at: :desc).last
+    streak_scope = Game.filter params
+    game = streak_scope.where("team1_id = ? OR team2_id = ?", team_id, team_id).last
     unless game.nil?
       if game.team1_id == team_id
-        last_loss = streak_scope.where(:team2_id => team_id).order(created_at: :desc).last
+        last_loss = streak_scope.where(:team2_id => team_id).last
         if last_loss.nil?
           streak = streak_scope.where("team1_id = ? OR team2_id = ?", team_id, team_id).count
         else
@@ -31,17 +67,4 @@ class Api::TeamStreaksController < ApplicationController
     end
     render :json => streak
   end
-
-  private
-    def scope(attributes)
-      attributes.inject(Game.order(created_at: :desc)) do |scope, (key, value)|
-        return scope if value.blank?
-        case key.to_sym
-        when :opponent
-          scope.where('team1_id = ? OR team2_id = ?', value, value)
-        else
-          scope
-        end
-      end
-    end
 end
