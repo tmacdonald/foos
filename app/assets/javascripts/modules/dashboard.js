b73b9f75151b4c2fbac50f11d6caf8dd096eb8c1
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-13 22:38:21 -0500

    Showing challenges in dashboard

--- app/assets/javascripts/modules/dashboard.js
+++ app/assets/javascripts/modules/dashboard.js
@@ -6,6 +6,16 @@ angular.module('foos.dashboard', [])
   }]);
 
 angular.module('foos.dashboard')
+  .controller('DashboardChallengesController', ['$scope', 'ChallengeService', 'Authentication', function($scope, Challenge, Auth) {
+
+    $scope.my_team = Auth.team();
+
+    Challenge.query({ challengee: $scope.my_team.id }).$promise.then(function(challenges) {
+      $scope.challenges = challenges;
+    });
+
+  }])
+
   .controller('StandingsController', ['$scope', 'TeamService', 'Authentication', function($scope, Team, Auth) {
     $scope.limit = 5;
 
