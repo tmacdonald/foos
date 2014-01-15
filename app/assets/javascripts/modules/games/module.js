a43d7e272e0ea92354ea637eae31813436911449
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-15 10:35:36 -0500

    Working on a better new game form

--- app/assets/javascripts/modules/games/module.js
+++ app/assets/javascripts/modules/games/module.js
@@ -1,6 +1,7 @@
 angular.module('foos.games', ['foos.games.controllers', 'foos.games.services'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
+      .when('/games/new/preview', { templateUrl: '/templates/games/form_new.html', controller: "NewGamePreviewController" })
       .when('/games/new', { templateUrl: '/templates/games/form.html', controller: "NewGameController" })
       .when('/games/simulate', { templateUrl: '/templates/games/simulate.html', controller: "GameSimulationController" })
       .when('/games/:team1_id/v/:team2_id', { templateUrl: '/templates/games/vs.html' })
