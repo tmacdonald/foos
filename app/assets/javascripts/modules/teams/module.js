bf80a83cf832d9e302c06269377c22ea443fe6ab
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-16 20:40:58 -0500

    Removed ladder link from navigation

--- app/assets/javascripts/modules/teams/module.js
+++ app/assets/javascripts/modules/teams/module.js
@@ -6,8 +6,7 @@ angular.module('foos.teams', ['foos.teams.controllers', 'foos.teams.services'])
       .when('/teams/:id', { templateUrl: '/templates/teams/show.html' })
       .when('/teams/:id/games', {templateUrl: '/templates/teams/games.html' })
       .when('/teams', { templateUrl: '/templates/teams/index.html' })
-      .when('/ladder', { templateUrl: '/templates/teams/ladder.html'})
-      .when('/rankings', { templateUrl: '/templates/teams/rankings.html' });
+      .when('/standings', { templateUrl: '/templates/teams/rankings.html' });
   }]);
 
 //= require modules/teams/controllers
