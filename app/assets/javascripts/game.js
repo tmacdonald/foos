839b7f57b4399fade42ebe4c0d0f06ccf588f2b4
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-22 11:18:01 -0500

    Completed first iteration of new game form

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,13 +1,42 @@
-var app = angular.module('foos.gameon', ['ngResource', 'ngAnimate', 'foos.authentication']);
+var app = angular.module('foos.gameon', ['ngResource', 'ngAnimate', 'foos.authentication', 'foos.teams.services', 'foos.games.services']);
 
-app.controller('GameOnController', ['$scope', function($scope) {
+app.controller('GameOnController', ['$scope', '$window', 'TeamService', 'GameService', function($scope, $window, Team, Game) {
   $scope.wizard = 1;
+  $scope.game = new Game();
+  $scope.game.team1score = 10;
 
-  $scope.next = function() {
-    $scope.wizard = $scope.wizard + 1;
+  Team.query().$promise.then(function(teams) {
+    $scope.teams = teams;
+  });
+
+  $scope.selectTeam1 = function(team1) {
+    $scope.team1 = team1;
+    $scope.game.team1_id = team1.id;
+    next();
   };
 
-  $scope.previous = function() {
-    $scope.wizard = $scope.wizard - 1;
+  $scope.selectTeam2 = function(team2) {
+    $scope.team2 = team2;
+    $scope.game.team2_id = team2.id;
+    next();
+  };
+
+  $scope.selectScore = function(score) {
+    $scope.game.team2score = score;
+    next();
+  };
+
+  function next() {
+    $scope.wizard = $scope.wizard + 1;
   }
+
+  $scope.confirm = function() {
+    $scope.game.$save().then(function() {
+      $window.location.href = '/';
+    });
+  }
+
+  $scope.getNumber = function(number) {
+    return new Array(number);
+  };
 }]);
