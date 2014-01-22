f2e441c0e3b6f8fcb21e52662795c2ce85798b8c
Tim Macdonald <macdonald.tim@gmail.com>
2014-01-22 14:39:45 -0500

    Updated new game screen to use ng-view and router

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,34 +1,38 @@
-var app = angular.module('foos.gameon', ['ngResource', 'ngAnimate', 'foos.authentication', 'foos.teams.services', 'foos.games.services']);
+var app = angular.module('foos.gameon', ['ngRoute', 'ngResource', 'ngAnimate', 'foos.authentication', 'foos.teams.services', 'foos.games.services'])
 
-app.controller('GameOnController', ['$scope', '$window', 'TeamService', 'GameService', function($scope, $window, Team, Game) {
-  $scope.wizard = 1;
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/winner', { templateUrl: '/templates/game/winner.html' })
+    .when('/loser', { templateUrl: '/templates/game/loser.html' })
+    .when('/score', { templateUrl: '/templates/game/score.html' })
+    .when('/confirm', { templateUrl: '/templates/game/confirm.html' })
+    .otherwise({ redirectTo: '/winner' });
+}]);
+
+app.controller('GameOnController', ['$scope', '$window', '$location', 'TeamService', 'GameService', function($scope, $window, $location, Team, Game) {
   $scope.game = new Game();
   $scope.game.team1score = 10;
 
-  Team.query().$promise.then(function(teams) {
+  Team.query({ order: '+name' }).$promise.then(function(teams) {
     $scope.teams = teams;
   });
 
   $scope.selectTeam1 = function(team1) {
     $scope.team1 = team1;
     $scope.game.team1_id = team1.id;
-    next();
+    $location.path('/loser');
   };
 
   $scope.selectTeam2 = function(team2) {
     $scope.team2 = team2;
     $scope.game.team2_id = team2.id;
-    next();
+    $location.path('/score');
   };
 
   $scope.selectScore = function(score) {
     $scope.game.team2score = score;
-    next();
+    $location.path('/confirm');
   };
-
-  function next() {
-    $scope.wizard = $scope.wizard + 1;
-  }
 
   $scope.confirm = function() {
     $scope.game.$save().then(function() {
--- app/assets/javascripts/modules/teams/controllers/team_new_controller.js
+++ app/assets/javascripts/modules/teams/controllers/team_new_controller.js
@@ -1,5 +1,5 @@
 angular.module('foos.teams.controllers')
-  .controller('TeamNewController', ['$scope', 'TeamService', function($scope, Team) {
+  .controller('TeamNewController', ['$scope', '$location', 'TeamService', function($scope, $location, Team) {
 
     $scope.team = new Team();
     
