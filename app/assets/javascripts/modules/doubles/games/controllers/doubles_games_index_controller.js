7f215b199f0bbce5ae4f47fb077864920c33570c
Tim Macdonald <macdonald.tim@gmail.com>
2014-03-14 18:16:50 -0400

    Can now see paginated view of all doubles games

---
+++ app/assets/javascripts/modules/doubles/games/controllers/doubles_games_index_controller.js
@@ -0,0 +1,17 @@
+angular.module('foos.doubles.games.controllers')  
+  .controller('DoublesGamesIndexController', ['$scope','$routeParams','$location','DoublesGameService', 'Authentication', function($scope, $routeParams, $location, Game, Auth) {
+    $scope.my_team = Auth.team();
+
+    $scope.page_size = 10;
+    $scope.page = parseInt($routeParams.page) || 1;
+
+    Game.query({order: '-played_at', offset: ($scope.page - 1) * $scope.page_size, limit: $scope.page_size}, function(games, headers) {
+      $scope.games = games;
+      $scope.total = headers('x-total-resources');
+      $scope.pages = Math.ceil($scope.total / $scope.page_size);
+    });
+
+    $scope.getNumber = function(number) {
+      return new Array(number);
+    };
+  }]);
--- app/assets/javascripts/modules/doubles/games/module.js
+++ app/assets/javascripts/modules/doubles/games/module.js
@@ -1,4 +1,7 @@
 angular.module('foos.doubles.games', ['foos.doubles.games.controllers', 'foos.doubles.games.services'])
-  
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/doubles/games', { templateUrl: '/templates/doubles/games/index.html' });
+  }]);
 //= require modules/doubles/games/controllers
 //= require modules/doubles/games/services
